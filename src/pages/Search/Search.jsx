9038cad60243297ca4ed7b67a74c1e4b93f3d012
AntoineCoignac <acoignac35@gmail.com>
2023-07-26 23:59:53 +0200

    debut search
    l'api ne retourne rien pour search user
    todo recuperer les infos user de chaque gig

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Back from '../../components/Back/Back';
 import "./Search.css";
 import Cross from '../../icons/cross/Cross';
 import NavSearch from '../../components/NavSearch/NavSearch';
-import { Link, useActionData } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ProfilePicture from '../../components/ProfilePicture/ProfilePicture';
 import LikeCounter from '../../components/LikeCounter/LikeCounter';
+import newRequest from '../../utils/newRequest';
 
 function Search() {
 
   const [isSearching, setIsSearching] = useState(false);
   const [type, setType] = useState("gig");
   const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
@@ -24,13 +26,57 @@ function Search() {
     setIsSearching(false);
   }
 
+  // Définir loadGigs en tant que callback memoisé
+  const loadGigs = async () => {
+    try {
+      const url = `/gigs?search=${search}`
+      const res = await newRequest.get(url);
+      setResult(res.data);
+      console.log(url);
+      console.log(res.data); // This should log the data fetched from the API
+    } catch (err) {
+      console.log(err);
+    }
+  }; 
+
+  // Définir loadUsers en tant que callback memoisé
+  const loadUsers = async () => {
+    try {
+      const url = `users/search-users?search=${encodeURIComponent(search)}`;
+      const res = await newRequest.get(url);
+      setResult(res.data);
+      console.log(url);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loadSearchResults = async () => {
+    setResult([]); // Clear the previous search results
+
+    if (type === "gig") {
+        if (search !== "") {
+            await loadGigs();
+        }
+    } else if (type === "user") {
+        if (search !== "") {
+            await loadUsers();
+        }
+    }
+};
+
+  useEffect(() => {
+    loadSearchResults();
+  }, [type, search]);
+
 
   return (
     <div className="search">
       <div className="top-bar">
         <Back />
         <div className='searchbar-ctn'>
-          <input onChange={handleSearch} defaultValue={search} className='searchbar' type="text" placeholder='Rechercher' />
+          <input onChange={handleSearch} value={search} className='searchbar' type="text" placeholder='Rechercher' />
           <button onClick={handleDelete} className={`delete ${isSearching ? "active" : ""}`}>
             <Cross />
           </button>
@@ -40,43 +86,32 @@ function Search() {
       {
         type === "gig" ? (
           <div className="gigs">
-            <div className='gig-result'>
-              <Link to={`gig/123`} className="gig">
-                <p className="title">Title</p>
-                <p className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat unde suscipit fugiat odit blanditiis saepe optio corporis reprehenderit vitae sed est accusamus quo hic, voluptatibus, exercitationem temporibus nihil quae aliquid fugit ex, tempore eligendi. Vero id explicabo repudiandae possimus suscipit nihil assumenda, natus rem ut quis, rerum in fuga exercitationem!</p>
-                <div className="infos">
-                  <span className="info">100€</span>
-                  <span className="info">6 jours</span>
-                  <span className="info">influenceur</span>
+            {
+              result ? (
+              result.map(gig=>(
+                <div key={gig._id} className='gig-result'>
+                  <Link to={`gig/${gig._id}`} className="gig">
+                    <p className="title">{gig.title}</p>
+                    <p className='desc'>{gig.desc}</p>
+                    <div className="infos">
+                      <span className="info">{gig.price}€</span>
+                      <span className="info">{gig.deliveryTime} jours</span>
+                      <span className="info">{gig.revisionNumber} modifications</span>
+                      <span className="info">{gig.tag}</span>
+                    </div>
+                  </Link>
+                  <div className="user">
+                    <Link to={`/creator/`}>
+                      <ProfilePicture photo="/img/pp/user1.jpg" badge="pro" />
+                    </Link>
+                    <LikeCounter nbr={12} />
+                  </div>
+                  <Link className='btn' to={`/work/chat/`}>Contacter Name</Link>
                 </div>
-              </Link>
-              <div className="user">
-                <Link to={`/creator/`}>
-                  <ProfilePicture photo="/img/pp/user1.jpg" badge="pro" />
-                </Link>
-                <LikeCounter nbr={12} />
-              </div>
-              <Link className='btn' to={`/work/chat/`}>Contacter Name</Link>
-            </div>
-            <div className='gig-result'>
-              <Link to={`gig/123`} className="gig">
-                <p className="title">Title</p>
-                <p className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat unde suscipit fugiat odit blanditiis saepe optio corporis reprehenderit vitae sed est accusamus quo hic, voluptatibus, exercitationem temporibus nihil quae aliquid fugit ex, tempore eligendi. Vero id explicabo repudiandae possimus suscipit nihil assumenda, natus rem ut quis, rerum in fuga exercitationem!</p>
-                <div className="infos">
-                  <span className="info">100€</span>
-                  <span className="info">6 jours</span>
-                  <span className="info">influenceur</span>
-                </div>
-              </Link>
-              <div className="user">
-                <Link to={`/creator/`}>
-                  <ProfilePicture photo="/img/pp/user1.jpg" badge="pro" />
-                </Link>
-                <LikeCounter nbr={12} />
-              </div>
-              <Link className='btn' to={`/work/chat/`}>Contacter Name</Link>
-            </div>
-          </div >
+              )
+              )) : null
+            }
+          </div>
         ) : (
           <div className="users">
             <Link className='user-result'>
